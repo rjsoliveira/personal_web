c56d212c445c4416d02fe14b0b0c77d648237932
roliveir <ricardo.oliveira@ec-lyon.fr>
2025-05-04 16:36:33 +0200

    Init

--- sections/about_me/education.py
+++ sections/about_me/education.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 st.title("Education")
 
-# st.html("<hr style=\"width:100%;text-align:left;margin-left:0\">")
+st.html("<hr style=\"width:100%;text-align:left;margin-left:0\">")
 
 # BACHELOR
 column1, column2 = st.columns(2, gap='small', vertical_alignment='center')
 with column1:
-    st.image("./sections/about_me/IST_logo.png", width=100)
+    st.image("./sections/about_me/IST_Logo.png", width=100)
 with column2:
     st.html("Bachelor in <b>Aerospace Engineering</b>.")
 
